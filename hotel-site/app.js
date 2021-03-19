e6ee6d421999a137ae1f1f7ef2b0d9d3896e073a
stian <stianhazel@gmail.com>
2021-03-19 18:47:32 +0000

    fix: browser UI on mobile shifting elements up and down

---
+++ hotel-site/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  document.ontouchmove = function(e){
+    e.preventDefault();
+  }
+
+  document.getElementsByTagName("input").onfocus = function () {
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+  }
+
+});
