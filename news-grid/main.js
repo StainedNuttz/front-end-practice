371702ecab49b501b44ac24005449fc263173ca8
Stian Hazelgrove <stianhazel@gmail.com>
2021-03-25 23:58:02 +0000

    Test fix

--- news-grid/main.js
+++ news-grid/main.js
@@ -6,9 +6,16 @@ document.querySelector('#showcase').style.setProperty('--vh', `${vh}px`);
 // so that the browser UI on mobile (firefox, safari for example) won't affect
 // the viewport dimensions upon the browser UI resizing (url bar showing for instance).
 // on desktops and laptops, the browser window could be resized and in that case we let the viewport dimensions change like normal.
+
+$(window).on(‘orientation’, updateViewheight);
+
 $(window).resize(function () {
   if (screen.width > 768) {
-    vh = window.innerHeight;
-    document.querySelector('#showcase').style.setProperty('--vh', `${vh}px`);
+    updateViewheight();
   }
 });
+
+function updateViewheight() {
+  vh = window.innerHeight;
+  document.querySelector('#showcase').style.setProperty('--vh', `${vh}px`);
+}
