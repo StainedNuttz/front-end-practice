02a7eec7cc1458ce4899f39849c8b11922a2068f
stainednuttz <stianhazel@gmail.com>
2021-04-01 00:16:16 +0100

    portfolio development continued

--- sass/sass-portfolio/dist/js/main.js
+++ sass/sass-portfolio/dist/js/main.js
@@ -11,3 +11,42 @@ $("a[class^='btn-']").on('click', function (event) {
     );
   }
 });
+
+const progressBars = document.querySelectorAll('.progress-bar > div');
+
+var progressBarWidths = new Map();
+// Save initial widths
+for (let i = 0; i < progressBars.length; i++) {
+  var current = progressBars[i];
+  progressBars[i].id = 'progressBar-' + i;
+  progressBarWidths.set('progressBar-' + i, current.style.width);
+  progressBars[i].style.width = 0;
+}
+
+const isInViewport = el => {
+  console.log("viewport check");
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+    (window.innerHeight ||
+      document.documentElement.clientHeight) &&
+    rect.right <=
+    (window.innerWidth ||
+      document.documentElement.clientWidth)
+  );
+};
+
+const run = () =>
+  progressBars.forEach(item => {
+    if (isInViewport(item)) {
+      item.classList.add('show');
+      item.style.width = progressBarWidths.get(item.id);
+    }
+  }
+);
+
+window.addEventListener('load', run);
+window.addEventListener('resize', run);
+window.addEventListener('scroll', run);
