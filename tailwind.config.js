b7f396e43429bde08db9b4d7b091826defb31e0d
stainednuttz <stianhazel@gmail.com>
2021-04-07 05:15:25 +0100

    tailwind configs + loan calculator UI redone + some functionaility

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      screens: {
+        'xs': '500px',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      opacity: ['disabled'],
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
