5c3dff96a26e8a6a8b4610caab76424b8312e492
stainednuttz <stianhazel@gmail.com>
2021-05-06 16:32:18 +0100

    finished UI

--- regex/app.js
+++ regex/app.js
@@ -7,81 +7,81 @@ const exp = document.querySelector('#exp');
 const reason = document.querySelector('#reason');
 const count = document.querySelector('#count');
 
-email.addEventListener('blur', () => {
-  const regex = /^([a-zA-Z0-9\-\.\_]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
-  if (!regex.test(email.value)) {
-    error(email, 'Invalid email');
-  } else {
-    success(email);
+const validations = {
+  'email': { 
+    'regex': /^([a-zA-Z0-9\-\.\_]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/,
+    'message': 'Please enter in a valid email address'
+  },
+  'username': {
+    'regex': /^[a-zA-Z0-9\.\_\-]{2,16}$/,
+    'message': 'Username must be between 2 and 16 characters and not contain any special characters'
+  },
+  'card': {
+    'regex': /^[0-9]{16}$/,
+    'message': 'Please enter in a valid card number'
+  },
+  'ccv': {
+    'regex': /^[0-9]{3}$/,
+    'message': 'Please enter in a valid CCV number'
+  },
+  'exp': { 
+    'regex': /^([1-9]|(10)|(11)|(12))\/\d{2}$/,
+    'message': 'Please enter a valid expiry date'
+  },
+  'reason': {
+    'regex': /^.{1,300}$/,
+    'message': 'Please enter in a message'
   }
-});
+}
 
-username.addEventListener('blur', () => {
-  const regex = /^[a-zA-Z0-9\.\_\-]{2,16}$/;
-  if (!regex.test(username.value)) { 
-    error(username, 'Username must be between 2 and 16 characters');
-  } else {
-    success(username);
-  }
-});
 
-card.addEventListener('blur', () => {
-  const regex = /^[0-9]{16}$/;
-  if (!regex.test(card.value)) { 
-    error(card, 'Invalid card number');
-  } else {
-    success(card);
-  }
-})
 
-ccv.addEventListener('blur', () => {
-  const regex = /^[0-9]{3}$/;
-  if (!regex.test(ccv.value)) { 
-    error(ccv, 'Invalid CCV');
-  } else {
-    success(ccv);
-  }
-});
+const inputBaseClass = 'resize-none border w-full rounded-sm p-1 focus:outline-none';
 
-exp.addEventListener('blur', () => {
-  const regex = /^([1-9]|(10)|(11)|(12))\/\d{2}$/;
-  if (!regex.test(exp.value)) { 
-    error(exp, 'Invalid date');
-  } else {
-    success(exp);
-  }
-});
+function validateInput(e) {
+  const ref = validations[e.id];
+  const prev = e.parentElement.querySelector('.msg-validate');
 
-reason.addEventListener('blur', () => {
-  const regex = /^.{1,300}$/;
-  if (!regex.test(reason.value)) { 
-    error(reason, 'Enter a message between 1 and 300 characters');
+  if (!ref.regex.test(e.value)) {
+    if (prev) { return; }
+    error(e, ref.message);
   } else {
-    success(reason);
+    if (prev) { prev.remove(); }
+    success(e);
   }
-});
+}
+
+function error(e, msg) {
+  const span = document.createElement('span');
+  span.className = 'msg-validate text-left text-red-600 text-xs mt-1';
+  span.textContent = msg;
+  e.parentElement.appendChild(span);
+
+  e.className = inputBaseClass + ' ring-1 border-red-600 ring-red-600';
+}
+
+function success(e) {
+  e.className = inputBaseClass + ' ring-1 border-green-300 ring-green-300';
+}
 
 reason.addEventListener('keyup', () => {
   count.textContent = `${reason.value.length} / 300 characters`;
 })
 
-const baseInput = 'resize-none border w-full rounded-sm p-1 focus:outline-none';
+document.querySelector('form').addEventListener('focusout', (e) => {
+  if (e.target.tagName !== 'BUTTON') { validateInput(e.target); }
+});
 
-function error(e, msg) {
-  const oldMsg = e.parentElement.querySelector('.msg-validate');
-  if (oldMsg) { oldMsg.remove(); }
+document.querySelector('form').addEventListener('focusin', (e) => {
+  const prev = document.querySelector('.msg-validate');
+  if (e.target.tagName !== 'BUTTON' && prev) { prev.remove(); }
+});
 
-  const span = document.createElement('span');
-  span.className = 'msg-validate text-left text-red-600 text-xs mt-1';
-  span.textContent = msg;
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
 
-  e.parentElement.appendChild(span);
-  e.className = baseInput + ' ring-1 border-red-600 ring-red-600';
-}
-
-function success(e) {
-  const msg = e.parentElement.querySelector('.msg-validate');
-  if (msg) { msg.remove(); }
-  e.className = baseInput + ' ring-1 border-green-300 ring-green-300';
-
-}
+  document.querySelectorAll('input').forEach((d) => {
+    validateInput(d);
+  });
+  validateInput(reason);
+});
